4648e2fa66eeebec5719f9dce99c270241a2180c
Marco Vanali <Marco91Vanali@gmail.com>
2018-03-08 22:04:10 +0000

    Link UI element to viewController.swift

--- WhatIsYourAge/ViewController.swift
+++ WhatIsYourAge/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var yearTextArea: UITextField!
+    @IBOutlet weak var findedAge: UILabel!
+    
+    @IBAction func buttonFindAge(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        
     }
 
     override func didReceiveMemoryWarning() {
